d19f97da7639a2ddf54a5c6f3b4e306a090ce19e
= <benjamin.schwab@duke.edu>
2015-04-16 16:31:48 -0400

    fixed callback

--- modules/SpotifyApiModule.js
+++ modules/SpotifyApiModule.js
@@ -1,6 +1,6 @@
 var client_id = '1adddf5d5b1c4e638dbcc6ea8f2c7503'; // Your client id
 var client_secret = '3a48222e35d04373b191bdd77344fb0c'; // Your client secret
-var redirect_uri = 'http://localhost:8888/callback'; // Your redirect uri
+var redirect_uri = 'http://still-depths-3109.herokuapp.com/callback'; // Your redirect uri
 
 var SpotifyWebApi = require('spotify-web-api-node');
 
