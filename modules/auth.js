70be70ec0247d2ef12109c90337beb9c77d21e1b
= <benjamin.schwab@duke.edu>
2015-04-16 16:37:56 -0400

    added redirect

--- modules/auth.js
+++ modules/auth.js
@@ -1,7 +1,7 @@
 auth = {
    client_id : '1adddf5d5b1c4e638dbcc6ea8f2c7503', // Your client id
    client_secret : '3a48222e35d04373b191bdd77344fb0c', // Your client secret
-   redirect_uri : 'http://localhost:8888/callback', // Your redirect uri
+   redirect_uri : 'http://still-depths-3109.herokuapp.com/callback', // Your redirect uri
   generateRandomString : function(length) {
   var text = '';
   var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
